4381b10c7523298c816c1c800632a70c9b7c23ce
MistehTimmeh <corybenrae@gmail.com>
2024-03-02 23:22:27 -0600

    Addition of the VideoGame model

---
+++ db/migrate/20240303052214_create_video_games.rb
@@ -0,0 +1,15 @@
+class CreateVideoGames < ActiveRecord::Migration[7.1]
+  def change
+    create_table :video_games do |t|
+      t.string :name
+      t.date :release_date
+      t.text :summary
+      t.decimal :metacritic_score
+      t.decimal :user_score
+      t.references :publisher, null: false, foreign_key: true
+      t.references :developer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
