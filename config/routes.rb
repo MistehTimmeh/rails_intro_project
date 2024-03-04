b6c97be79e86045d99aa2d438dfdb9cf38332433
MistehTimmeh <corybenrae@gmail.com>
2024-03-03 22:54:22 -0600

    Laying groundwork for search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :about, only: [:index]
-  resources :video_games, only: [:index, :show]
-  resources :publishers, only: [:index, :show]
-  resources :developers, only: [:index, :show]
-  resources :platforms, only: [:index, :show]
+  resources :video_games, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
+  resources :publishers, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
+  resources :developers, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
+  resources :platforms, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
